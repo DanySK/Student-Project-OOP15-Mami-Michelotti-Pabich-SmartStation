403a1cddb784be5358f0b8aeac768aab1739ff4e
Alessandro Mami <alessandro.mami2@studio.unibo.it>
2016-08-21 23:45:50 +0200

    Updating test, but we have some problems.

--- src/application/model/test/MyTest.java
+++ src/application/model/test/MyTest.java
@@ -75,23 +75,32 @@ public class MyTest {
      */
     public void testFuelManager(final FuelManager fManager) {       
         //Insert a new fuel, testing if it's there and if i can get it
-        assertNotNull(fManager.getFuel("testFuel"));
+        fManager.addFuel("testFuel", 125, 100, new Color(10, 10, 10, 1));
+        fManager.getFuel("testFuel");
         assertNotNull(fManager.getAllFuels());
         assertEquals("there must be only one pump", 1, fManager.getAllFuels().size());
         
         //Insert second fuel, testing if it's there and if i can get it
+        fManager.addFuel("testFuel2", 125, 100, new Color(11, 11, 11, 1));
         assertNotNull(fManager.getFuel("testFuel2"));
         assertNotNull(fManager.getAllFuels());
         assertEquals("there must be 2 pumps", 2, fManager.getAllFuels().size());
         
         //Removing the second fuel
+        fManager.removeFuel("testFuel2");
         assertNull(fManager.getFuel("testFuel2"));
         assertEquals("there must be only 1 pump", 1, fManager.getAllFuels().size());
         
         //Reinsert the second fuel
+        fManager.addFuel("testFuel2", 125, 100, new Color(11, 11, 11, 1));
         assertNotNull(fManager.getFuel("testFuel2"));
         assertNotNull(fManager.getAllFuels());
         assertEquals("there must be 2 pumps", 2, fManager.getAllFuels().size());
+        
+        //Testing not permitted inserts
+        //assertFalse(Manager.addFuel("testFuel2", 125, 100, new Color(11, 11, 11, 1)));
+        //assertFalse(Manager.addFuel("", 125, 100, new Color(15, 15, 15, 1)));
+        //assertFalse(Manager.addFuel("testFuel3", 125, 100, new Color(11, 11, 11, 1)));
         
         //Testing for getters
         assertEquals("the name should be testFuel", "testFuel", fManager.getFuel("testFuel").getName());
@@ -100,8 +109,14 @@ public class MyTest {
         assertEquals("the wholesale price should be 100", 100, fManager.getFuel("testFuel").getWholeSalePrice());
         
         //Testing for setters
+        //assertFalse(fManager.getFuel("testFuel").setName("testFuel2"));
+        fManager.getFuel("testFuel").setName("testFuel3");
+        fManager.getFuel("testFuel3").setName("testFuel");
+        fManager.getFuel("testFuel").setPrice(150);
         assertEquals("the price should be 150", 150, fManager.getFuel("testFuel").getPrice());
+        fManager.getFuel("testFuel").setWholeSalePrice(200);
         assertEquals("the wholesale price should be 200", 200, fManager.getFuel("testFuel").getWholeSalePrice());
+        fManager.getFuel("testFuel").setColor(new Color(100, 100, 100, 1));
     }
     
     
@@ -111,36 +126,47 @@ public class MyTest {
      * @param fManager Fuel manager from main model
      */
     public void testReserveManager(final ReserveManager rManager, final FuelManager fManager) {
-        //Adding first reserve and testing if it's there     
+        //Adding first reserve and testing if it's there
+        rManager.addReserve(1000, 1000, 2000, 10, fManager.getFuel("testFuel"), 50000, 1000);           
         assertEquals("the reserve type should be testFuel", "testFuel", rManager.getReserve(fManager.getFuel("testFuel")).getType().getName());
         assertNotNull(rManager.getAllReserves());
         assertEquals("the reserve count should be 1", 1, rManager.getAllReserves().size());
         
         //Adding second reserve and testing if all are there
+        rManager.addReserve(1000, 1000, 2000, 10, fManager.getFuel("testFuel2"), 50000, 1000);
         assertEquals("the reserve type should be testFuel2", "testFuel2", rManager.getReserve(fManager.getFuel("testFuel2")).getType().getName());
         assertNotNull(rManager.getAllReserves());
         assertEquals("the reserve count should be 2", 2, rManager.getAllReserves().size());
         
         //Remove and test
+        rManager.removeReserve(rManager.getReserve(fManager.getFuel("testFuel2")));
         assertEquals("the reserve count should be 1", 1, rManager.getAllReserves().size());
         
         //Reinsert and test
+        rManager.addReserve(1000, 1000, 2000, 10, fManager.getFuel("testFuel2"), 50000, 1000);
         assertEquals("the reserve count should be 2", 2, rManager.getAllReserves().size());
-       
+        
+        //Not permitted inserts
+        //assertFalse(rManager.addReserve(1000, 1000, 2000, 10, fManager.getFuel("testFuel"), 50000, 1000));
+        //assertFalse(rManager.addReserve(1000, 1000, 2000, 10, fManager.getFuel("testFuel2"), 50000, 1000));
+        
         //Setter and getter tester
+        rManager.getReserve(fManager.getFuel("testFuel")).setCapacity(55000);
         assertEquals("the capacity should be 55000", 55000, rManager.getReserve(fManager.getFuel("testFuel")).getCapacity());
         
-        //assertTrue(rManager.getReserve(fManager.getFuel("testFuel")).setCost(2500));
+        rManager.getReserve(fManager.getFuel("testFuel")).setCost(2500);
         assertEquals("the cost should be 2500", 2500, rManager.getReserve(fManager.getFuel("testFuel")).getCost());
         
-        //assertTrue(rManager.getReserve(fManager.getFuel("testFuel")).setMaxDurability(1500));
+        rManager.getReserve(fManager.getFuel("testFuel")).setMaxDurability(1500);
         assertEquals("the max durability should be 1500", 1500, rManager.getReserve(fManager.getFuel("testFuel")).getMaxDurability());
         
-        //assertTrue(rManager.getReserve(fManager.getFuel("testFuel")).setRemaining(200));
+        rManager.getReserve(fManager.getFuel("testFuel")).setRemaining(200);
         assertEquals("the remaining should be 200", 200, rManager.getReserve(fManager.getFuel("testFuel")).getRemaining());
         
-        //assertTrue(rManager.getReserve(fManager.getFuel("testFuel")).setRepairCost(1));
+        rManager.getReserve(fManager.getFuel("testFuel")).setRepairCost(1);
         assertEquals("the repair cost should be 1", 1, rManager.getReserve(fManager.getFuel("testFuel")).getRepairCost());
+        
+        //assertFalse(rManager.getReserve(fManager.getFuel("testFuel")).setType(fManager.getFuel("testFuel2")));
     }
 
     
@@ -150,34 +176,47 @@ public class MyTest {
      */
     public void testPumpManager(final PumpManager pManager) {
         //Insert the first pump and testing if it's there
+        //pManager.addPump(1000, 1000, 2000, 10, "pumpTest", fManager.getFuel("testFuel"), 100);
         assertNotNull(pManager.getPumpByName("pumpTest"));
         assertNotNull(pManager.getPump(0));
         assertNotNull(pManager.getAllPumps());
         assertEquals("there must be only 1 pump", 1, pManager.getAllPumps().size());
         
         //Insert the second pump and testing if it's there
+        //pManager.addPump(1000, 1000, 2000, 10, "pumpTest2", fManager.getFuel("testFuel2"), 100);
         assertNotNull(pManager.getPumpByName("pumpTest2"));
         assertNotNull(pManager.getPump(1));
         assertNotNull(pManager.getAllPumps());
         assertEquals("there must be 2 pumps", 2, pManager.getAllPumps().size());
+        
+        //Not permitted inserts
+        //assertFalse(pManager.addPump(1000, 1000, 2000, 10, "pumpTest", fManager.getFuel("testFuel"), 100));
+        //assertFalse(pManager.addPump(1000, 1000, 2000, 10, "pumpTest2", fManager.getFuel("testFuel2"), 100));
               
         //Removing pump and reinsert it
+        pManager.removePump(pManager.getPumpByName("pumpTest2"));
         assertEquals("there must be 1 pump", 1, pManager.getAllPumps().size());
+        //assertTrue(pManager.addPump(1000, 1000, 2000, 10, "pumpTest2", fManager.getFuel("testFuel2"), 100));
         assertEquals("there must be 2 pumps", 2, pManager.getAllPumps().size());
         
+        pManager.getPumpByName("pumpTest").setName("pumpTest3");
         assertNotNull(pManager.getPumpByName("pumpTest3"));
         assertEquals("the name should be pumpTest3", "pumpTest3", pManager.getPumpByName("pumpTest3").getName());
         
         //Testing getters and setters
+        pManager.getPumpByName("pumpTest3").setMaxDurability(1500);
         assertEquals("the max durability should be 1500", 1500, pManager.getPumpByName("pumpTest3").getMaxDurability());
         
+        pManager.getPumpByName("pumpTest3").setCost(2500);
         assertEquals("the cost should be 2500", 2500, pManager.getPumpByName("pumpTest3").getCost());
         
+        pManager.getPumpByName("pumpTest").setRepairCost(15);
         assertEquals("the repair cost should be 15", 15, pManager.getPumpByName("pumpTest3").getRepairCost());
         
         pManager.getPumpByName("pumpTest3").consume();
         assertEquals("the durability should be 1999", 1999, pManager.getPumpByName("pumpTest3").getDurability());
         
+        pManager.getPumpByName("pumpTest3").setSpeed(150);
         assertEquals("the speed should be 150", 150, pManager.getPumpByName("pumpTest3").getSpeed());
     }
     
@@ -222,6 +261,7 @@ public class MyTest {
         
         //Testing the occupy of the area
         assertTrue(aManager.getArea(3, 3).isOccupied());
+        assertNotNull(aManager.getArea(3, 3).getVehicle());
     }
     
     /**
@@ -229,6 +269,11 @@ public class MyTest {
      * @param mManager Money Manager from the main model
      */
     public void testMoneyManager(final MoneyManager mManager) {
+        
+        //Adding some movements
+        mManager.addMovement(MovementType.BUILD, 2500, "test description");
+        mManager.addMovement(MovementType.REPAIR, 2500, "test description");
+        mManager.addMovement(MovementType.BUILD, 5000, "test description");
         
         //Controlling number of movements
         assertNotNull(mManager.getAllMovements());
@@ -244,6 +289,9 @@ public class MyTest {
      * @param fManager fuel manager from the main model
      */
     public void testConsumers(final Vehicle vehicle, final FuelManager fManager) {
+        //Controlling setters
+        vehicle.setRequest(100);
+        vehicle.setFuel(fManager.getFuel("testFuel"));
         
         //Controlling getters
         assertEquals("the request should be 100", 100, vehicle.getRequest());
